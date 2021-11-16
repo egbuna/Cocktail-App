d676735bdff35950ed76a45dbda90244e8c20518
egbuna <egbunachukwudi49@gmail.com>
2021-11-16 17:50:49 +0100

    search for cocktail

--- app/src/main/java/com/egbuna/cocktailapp/presentation/cocktaillist/CocktailListViewModel.kt
+++ app/src/main/java/com/egbuna/cocktailapp/presentation/cocktaillist/CocktailListViewModel.kt
@@ -4,22 +4,32 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.egbuna.cocktailapp.common.Resource
 import com.egbuna.cocktailapp.domain.model.CocktailListState
 import com.egbuna.cocktailapp.domain.model.SearchCocktailState
 import com.egbuna.cocktailapp.domain.usecase.GetCocktails
+import com.egbuna.cocktailapp.domain.usecase.SearchCocktail
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CocktailListViewModel @Inject constructor(private val getCocktails: GetCocktails): ViewModel() {
+class CocktailListViewModel @Inject constructor(
+    private val getCocktails: GetCocktails,
+    private val searchCocktail: SearchCocktail
+): ViewModel() {
     private val _state = mutableStateOf(CocktailListState())
     val state: State<CocktailListState> = _state
 
     private val _searchState = mutableStateOf(SearchCocktailState())
     val searchState: State<SearchCocktailState> = _searchState
+
+    private var searchJob: Job? = null
 
     init {
        getCocktails()
@@ -28,8 +38,26 @@ class CocktailListViewModel @Inject constructor(private val getCocktails: GetCoc
     fun onTextChange(value: String) {
         if (value.isEmpty()) {
             _searchState.value = searchState.value.copy(isHintVisible = true, searchWord = "")
+            getCocktails()
         } else {
             _searchState.value = searchState.value.copy(isHintVisible = false, searchWord = value)
+            searchJob?.cancel()
+            searchJob = viewModelScope.launch {
+                delay(1000)
+                searchCocktail.invoke(value).onEach { result ->
+                    when(result) {
+                        is Resource.Loading -> {
+                            _state.value = CocktailListState(isLoading = true)
+                        }
+                        is Resource.Success -> {
+                            _state.value = CocktailListState(success = result.data ?: emptyList(), isLoading = false)
+                        }
+                        is Resource.Error -> {
+                            _state.value = CocktailListState(error = result.message, isLoading = false)
+                        }
+                    }
+                }.launchIn(viewModelScope)
+            }
         }
     }
 
--- app/src/main/java/com/egbuna/cocktailapp/presentation/cocktaillist/component/CocktailItem.kt
+++ app/src/main/java/com/egbuna/cocktailapp/presentation/cocktaillist/component/CocktailItem.kt
@@ -1,26 +1,17 @@
 package com.egbuna.cocktailapp.presentation.cocktaillist.component
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.rememberImagePainter
 import com.egbuna.cocktailapp.domain.model.Cocktail
 
@@ -31,45 +22,40 @@ fun CocktailListItem(
     onFavouriteItemClick: (Cocktail) -> Unit,
     cocktail: Cocktail
 ) {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier.padding(horizontal = 2.dp, vertical = 4.dp)
-    ) {
+    CocktailImageContainer() {
         Box(
             modifier = modifier
-                .width(180.dp)
-                .height(230.dp)
                 .clickable {
                     onClick.invoke()
                 }
 
         ) {
             Image(
-                contentScale = ContentScale.FillBounds,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxSize(),
-                painter = rememberImagePainter(cocktail.drinkThumb), contentDescription = "Cocktail image"
+                painter = rememberImagePainter(cocktail.drinkThumb, builder = {
+                    crossfade(true)
+                }), contentDescription = "Cocktail image"
             )
             Text(text = cocktail.drink, color = Color.White, modifier = Modifier
                 .padding(start = 16.dp, top = 16.dp, end = 16.dp)
                 .fillMaxWidth(),
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis)
-//            IconButton(
-//                modifier = Modifier.constrainAs(iconButton) {
-//                    top.linkTo(parent.top, margin = 8.dp)
-//                    end.linkTo(parent.end)
-//                },
-//                onClick = {
-//                          onFavouriteItemClick.invoke(cocktail)
-//                },
-//            ) {
-//                Icon(
-//                    imageVector = if (cocktail.favourited) Icons.Default.Favorite else Icons.Default.FavoriteBorder,tint = Color.White,
-//                    contentDescription = "Favourite"
-//                )
-//            }
-            Spacer(modifier = Modifier.height(16.dp))
         }
+    }
+}
+
+@Composable
+fun CocktailImageContainer(content: @Composable () -> Unit) {
+    Surface(
+        Modifier
+            .width(180.dp)
+            .height(230.dp)
+            .padding(top = 16.dp, end = 8.dp),
+        RoundedCornerShape(8.dp)
+    ) {
+        content()
     }
 }
